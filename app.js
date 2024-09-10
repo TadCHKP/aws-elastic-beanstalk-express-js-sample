b7d460546d38d6547b1514fc4a5da1b7e3e9bfb8
Tad Helke <tadh@checkpoint.com>
2024-09-10 07:11:22 -0700

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Tad, you massive stud!!'));
+app.get('/', (req, res) => res.send('Hello Tad, you updated this on the second day!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
